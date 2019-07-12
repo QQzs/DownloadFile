994ac4a89db60bcb7b54ec70a1d0df71470080d1
CodeZhang <552146383@qq.com>
2019-07-12 17:29:14 +0800

    权限申请

--- app/src/main/java/com/zs/demo/downloadfile/MainActivity.java
+++ app/src/main/java/com/zs/demo/downloadfile/MainActivity.java
@@ -1,30 +1,72 @@
 package com.zs.demo.downloadfile;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RxPermissions mRxPermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRxPermission = new RxPermissions(this);
     }
 
     public void singleDownload(View view){
-        Intent intent = new Intent(this,SingleActivity.class);
-        startActivity(intent);
+
+        mRxPermission.request(Manifest.permission.READ_EXTERNAL_STORAGE , Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean aBoolean) throws Exception {
+                        if (aBoolean){
+                            Intent intent = new Intent(MainActivity.this,SingleActivity.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(MainActivity.this , "请打开读写权限" , Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     public void multipleDownload(View view){
-        Intent intent = new Intent(this,MultipleActivity.class);
-        startActivity(intent);
+        mRxPermission.request(Manifest.permission.READ_EXTERNAL_STORAGE , Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean aBoolean) throws Exception {
+                        if (aBoolean){
+                            Intent intent = new Intent(MainActivity.this,MultipleActivity.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(MainActivity.this , "请打开读写权限" , Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     public void limitDownload(View view){
-        Intent intent = new Intent(this,LimitActivity.class);
-        startActivity(intent);
+        mRxPermission.request(Manifest.permission.READ_EXTERNAL_STORAGE , Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean aBoolean) throws Exception {
+                        if (aBoolean){
+                            Intent intent = new Intent(MainActivity.this,LimitActivity.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(MainActivity.this , "请打开读写权限" , Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
+
 }
